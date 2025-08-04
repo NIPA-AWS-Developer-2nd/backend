dbf43e2e926ec8cfaaf4d6c7a85b720bc3938d62
jungminchae <minchae3618@gmail.com>
2025-08-04 17:01:32 +0900

    test를 위한 코드 삭제

--- src/aws/aws.module.ts
+++ src/aws/aws.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
 import { S3Service } from './s3.service';
-import { AwsController } from './aws.controller';
 
 @Module({
-  controllers: [AwsController],
   providers: [S3Service],
   exports: [S3Service],
 })
