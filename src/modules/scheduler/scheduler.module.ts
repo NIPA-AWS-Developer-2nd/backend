9eb7d363cf8795c3470daef22f01c18c007a5a86
Jaewoo Kim <164657817+nullisdefined@users.noreply.github.com>
2025-08-16 21:46:30 +0900

    fix: DI

--- src/modules/scheduler/scheduler.module.ts
+++ src/modules/scheduler/scheduler.module.ts
@@ -2,16 +2,16 @@ import { Module } from '@nestjs/common';
 import { ScheduleModule as NestScheduleModule } from '@nestjs/schedule';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MeetingSchedulerService } from './meeting-scheduler.service';
-import { Meeting, MeetingParticipant } from '../../entities';
+import { Meeting, MeetingParticipant, MeetingAttendance } from '../../entities';
 import { PointModule } from '../point/point.module';
 
 @Module({
   imports: [
     NestScheduleModule.forRoot(),
-    TypeOrmModule.forFeature([Meeting, MeetingParticipant]),
+    TypeOrmModule.forFeature([Meeting, MeetingParticipant, MeetingAttendance]),
     PointModule,
   ],
   providers: [MeetingSchedulerService],
-  exports: [MeetingSchedulerService]
+  exports: [MeetingSchedulerService],
 })
 export class SchedulerModule {}
